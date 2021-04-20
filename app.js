9fdbd6b55e91ff31902fd7ba96d9c2bf5df0c739
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-04-20 12:20:03 +0530

    data fetching from api

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const ejs = require("ejs");
+const https = require("https");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -8,10 +12,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-    res.render("home");
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.listen("3000", function(){
-    console.log("Server started running at port 3000.");
+app.post("/", function (req, res) {
+  const filmName = req.body.title;
+  const apiKey = process.env.API;
+  const mainUrl = process.env.URL;
+  const url = mainUrl + apiKey + "&t=" + filmName;
+  https.get(url, function (response) {
+    const chunks = [];
+    response
+      .on("data", function (data) {
+        chunks.push(data);
+      })
+      .on("end", function () {
+        const data = Buffer.concat(chunks);
+        const schema = JSON.parse(data);
+        const response = schema.Response;
+        if (response === "True") {
+          const title = schema.Title;
+          const posterUrl = schema.Poster;
+          const released = schema.Released;
+          const genre = schema.Genre;
+          const director = schema.Director;
+          const actors = schema.Actors;
+          const plot = schema.Plot;
+          const language = schema.Language;
+          const imdbRating = schema.imdbRating;
+          res.render("details", {
+            title: title,
+            posterUrl: posterUrl,
+            released: released,
+            genre: genre,
+            director: director,
+            actors: actors,
+            plot: plot,
+            imdbRating: imdbRating,
+            language: language,
+          });
+        } else {
+          res.render("failure");
+        }
+      });
+  });
+});
+
+app.listen("3000", function () {
+  console.log("Server started running at port 3000.");
 });
