c5b54db6f11dfcf23b347525d5244a17b549ad91
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-05-01 18:06:20 +0530

    deploying to web

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.get("/contact", function (req, res) {
   res.render("contact");
 });
 
-app.listen("3000", function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started running at port 3000.");
 });
